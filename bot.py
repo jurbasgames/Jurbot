30a889ef4c4dbd0ee3c67ff80816fbf4c752ce75
João Pedro Duarte <jurbasgames@gmail.com>
2022-06-23 23:34:01 -0300

    update: API changed from waifus to random images

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+from zoneinfo import ZoneInfo
 import requests
 from datetime import datetime
 from discord.ext import commands
@@ -7,28 +8,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.environ['DISCORD_TOKEN']
+API_KEY = os.environ['API_KEY']
 
 client = commands.Bot(command_prefix='?')
 
+
 @client.event
 async def on_ready():
-    
+
     print(f'{client.user} is connected to the following guilds:\n')
     for guild in client.guilds:
         print(f'{guild}\n')
 
+
 @client.command()
 async def quarta(ctx):
-    if datetime.now().strftime('%A') != 'Wednesday':
+    if datetime.now(ZoneInfo("America/Sao_Paulo")).strftime('%A') != 'Wednesday':
         await ctx.send('Hoje definitivamente não é quarta-feira meus bacanos')
     else:
         await ctx.send('Hoje é quarta-feira meus bacanos!!')
 
 
 @client.command()
-async def waifu(ctx):
-    image = requests.get(('https://api.waifu.pics/sfw/waifu'))
-    await ctx.send(f'{image.json()["url"]}')
+async def random(ctx):
+    image = requests.get(
+        url=f'https://api.unsplash.com/photos/random/?client_id={API_KEY}')
+    await ctx.send(image.json()["urls"]["raw"])
 
 
 @client.event
